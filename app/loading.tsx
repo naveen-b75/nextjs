e9af552bce7df2918f2abf8a034d7e4bb46de066
naveen-b75 <104362649+naveen-b75@users.noreply.github.com>
2024-08-09 19:45:02 +0530

    Add files via upload

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import Grid from 'components/grid';
+
+export default function Loading() {
+  return (
+    <Grid>
+      <Grid.Item className="animate-pulse bg-gray-100 dark:bg-gray-900" />;
+    </Grid>
+  );
+} 
