d0a3cba9788b779b0c8f66af33859f49947fce4c
naveen-b75 <104362649+naveen-b75@users.noreply.github.com>
2024-09-05 19:15:12 +0530

    Update index.tsx

--- magentoComponents/productGallery/index.tsx
+++ magentoComponents/productGallery/index.tsx
@@ -1,15 +1,24 @@
 'use client';
+import Button from 'components/Button';
+import { getCollectionProductsProxy } from 'lib/magento';
 import { MagentoProductsFilter, MagentoVercelProduct } from 'lib/magento/types';
 import { default as MagentoGrid } from 'magentoComponents/grid';
 import { default as MagentoProductGridItems } from 'magentoComponents/layout/product-grid-items';
 import { LayeredNav as MagentoLayeredNav } from 'magentoComponents/layout/search/filter/LayeredNav';
-import { default as MagentoPagination } from 'magentoComponents/layout/search/filter/pagination';
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useCallback, useState } from 'react';
 
 export default function MagentoProductGallery({
   products,
-  searchParams
+  collection,
+  searchParams,
+  transformedFilters,
+  pageSize,
+  currentPage,
+  searchValue,
+  sort
 }: {
+  pageSize: number;
   products: {
     items: MagentoVercelProduct[];
     sort_fields: {
@@ -21,6 +30,13 @@ export default function MagentoProductGallery({
     total_count: number;
     aggregations: MagentoProductsFilter[];
   };
+  sort: {
+    [key: string]: string;
+  };
+  collection?: string;
+  transformedFilters: any;
+  currentPage: string;
+  searchValue?: string;
   searchParams:
     | {
         [key: string]: string | undefined;
@@ -28,12 +44,49 @@ export default function MagentoProductGallery({
     | undefined;
 }) {
   const [isLoading, setIsLoading] = useState<boolean>(false);
-
+  const [displayProducts, setDisplayProducts] = useState<{
+    items: MagentoVercelProduct[];
+    sort_fields: {
+      options: {
+        label: string;
+        value: string;
+      }[];
+    };
+    total_count: number;
+    aggregations: MagentoProductsFilter[];
+  }>(products);
   const [isActive, setIsActive] = useState(false);
+  const [pageNo, setPageNo] = useState<string>(searchParams?.['page'] || '1');
+  const router = useRouter();
 
   const toggleActiveClass = () => {
     setIsActive(!isActive);
   };
+
+  const loadMoreProducts = useCallback(async (pageNo: number) => {
+    try {
+      setIsLoading(true);
+      if (collection) {
+        const response = await getCollectionProductsProxy({
+          collection: collection,
+          filters: transformedFilters,
+          pageSize: pageSize * pageNo,
+          currentPage: 1,
+          sort: sort
+        });
+        // Get the current URL and append the page query parameter
+        const searchParams = new URLSearchParams(window.location.search);
+        searchParams.set('page', pageNo.toString());
+        router.push(`${window.location.pathname}?${searchParams.toString()}`, { scroll: false });
+        setPageNo(pageNo.toString());
+        setDisplayProducts(response.body);
+        setIsLoading(false);
+      }
+    } catch (e) {
+      setIsLoading(false);
+    }
+  }, []);
+
   return (
     <section>
       {isLoading && (
@@ -88,7 +141,7 @@ export default function MagentoProductGallery({
           </span>
           <MagentoLayeredNav
             setIsLoading={setIsLoading}
-            filtersData={products?.aggregations}
+            filtersData={displayProducts?.aggregations}
             searchParams={searchParams}
           />
         </div>
@@ -99,11 +152,30 @@ export default function MagentoProductGallery({
           >
             Filter
           </button>
-          <h2 className="mb-[10px]">{products.total_count} Results</h2>
+          <h2 className="mb-[10px]">{displayProducts.total_count} Results</h2>
           <MagentoGrid className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-            <MagentoProductGridItems products={products.items} />
+            <MagentoProductGridItems products={displayProducts.items} />
           </MagentoGrid>
-          <MagentoPagination TotalProducts={products.total_count} searchParams={searchParams} />
+          {displayProducts.total_count > displayProducts?.items?.length && (
+            <div className="text-center">
+              {' '}
+              <Button
+                type="button"
+                size="small"
+                priority="secondary"
+                disabled={isLoading}
+                onClick={() => {
+                  loadMoreProducts(parseInt(pageNo) + 1);
+                }}
+                classes={{
+                  root: 'mt-[62px] rounded-[6px] border-[2px] border-teal px-[32px] py-[12px] bg-white hover:bg-white',
+                  content: 'font-bold !text-teal text-[14px]'
+                }}
+              >
+                {isLoading ? 'Loading...' : 'Load More'}
+              </Button>
+            </div>
+          )}
         </div>
       </section>
     </section>
